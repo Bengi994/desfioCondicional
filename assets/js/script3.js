7d4d1a3bd5943a1adaab35ef08ac99f5a41618f9
Bengi994 <88071990+Bengi994@users.noreply.github.com>
2024-06-09 21:58:54 -0500

    sin funciones jeje

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,10 +1,9 @@
-document.getElementById('submit').addEventListener('click', function() {
-    const digit1 = document.getElementById('digit1').value;
-    const digit2 = document.getElementById('digit2').value;
-    const digit3 = document.getElementById('digit3').value;
-    const password = digit1 + digit2 + digit3;
-
-    let resultText;
+document.getElementById('submit').onclick = function() {
+    var digit1 = document.getElementById('digit1').value;
+    var digit2 = document.getElementById('digit2').value;
+    var digit3 = document.getElementById('digit3').value;
+    var password = digit1 + digit2 + digit3;
+    var resultText;
     if (password === '911') {
         resultText = 'password 1 correcto';
     } else if (password === '714') {
@@ -12,6 +11,5 @@ document.getElementById('submit').addEventListener('click', function() {
     } else {
         resultText = 'password incorrecto';
     }
-
     document.getElementById('result').textContent = resultText;
-});
+};
